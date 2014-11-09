6a07d36a67fcf7ddb3e957b21fd00feb83cff755
Dmidd87 <dmidd87@gmail.com>
2014-11-09 13:43:38 -0700

    Fixed one error message now I need a table?

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,5 +1,3 @@
 class Registration < ActiveRecord::Base
 
-attr_accessor :first_name, :last_name, :email_address, :password, :password_confirmation
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ resources :pages
 resources :faq
 resources :registrations
 resources :services
-resources :user
+resources :users
 
-get '/sign-up' => 'registrations#new', as: :signup
-post '/sign-up' => 'registrations#create'
+# get '/sign-up' => 'registrations#new', as: :signup
+# post '/sign-up' => 'registrations#create'
 
 end
