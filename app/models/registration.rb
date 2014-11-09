58b93fc7cb9da32fb51890121690ba6d750d0e14
Dmidd87 <dmidd87@gmail.com>
2014-11-09 10:03:52 -0700

    CRUD form on sign up wasn't working so I deleted it and am ststarting that controller and view from scratch

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,3 +1,5 @@
 class Registration < ActiveRecord::Base
 
+attr_accessor :first_name, :last_name, :email_address, :password, :password_confirmation
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
 
+root "pages#index"
+
 resources :pages
 resources :faq
 resources :registrations
+resources :services
+resources :user
 
-root "pages#index"
-
+get '/sign-up' => 'registrations#new', as: :signup
+post '/sign-up' => 'registrations#create'
 
 end
